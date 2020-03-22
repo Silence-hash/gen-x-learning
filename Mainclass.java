c8dea05b3a2d5276fa125520d08fd9d8ec14695b
nursyahranirani <nursyahranirani05@gmail.com>
2020-03-23 02:41:23 +0800

    Program CLI menghitung luas by: agus kurniawan

---
+++ Mainclass.java
@@ -0,0 +1,29 @@
+
+package Tugas;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Mainclass {
+
+ 
+    public static void main(String[] args) {
+            System.out.println("###############################");
+	    System.out.println("##  ##### ####  #   # #####  ##");
+	    System.out.println("##  #   # #     #   # #      ##");
+	    System.out.println("##  ##### ##### #   # #####  ##");
+	    System.out.println("##  #   # #   # #   #     #  ##");
+	    System.out.println("##  #   # ##### ##### #####  ##");
+	    System.out.println("###############################");
+            
+                      System.out.println("### WELCOME TO TUGASKU WKWKW ##");
+                      
+                      Input inputt = new Input(); 
+                      
+                      inputt.Pertambahan();
+
+                      
+    }
+    
+}
