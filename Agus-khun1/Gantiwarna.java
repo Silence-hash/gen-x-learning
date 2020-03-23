6ccb8ba06e041952ff0b13faf7668f5e3bb4513c
nursyahranirani <nursyahranirani05@gmail.com>
2020-03-24 00:21:09 +0800

    Pertukaran warna

---
+++ Agus-khun1/Gantiwarna.java
@@ -0,0 +1,50 @@
+
+package Agus-khun1;
+
+
+public class Gantiwarna {
+    private String nama1;
+    private String nama2;
+    private String warna1;
+    private String warna2;
+    
+    private String simpansementara1;
+    private String simpansementara2;
+
+    public String getNama1(){
+        return this.nama1;
+    }
+    
+    public void setNama1(String nama1){
+        this.nama1 = nama1;
+    }
+    public String getNama2(){
+        return this.nama2;
+    }
+    
+    public void setNama2(String nama2){
+        this.nama2 = nama2;
+    }
+    
+    public  String getWarna1(){
+        return this.warna2;
+    }
+    
+    public void setWarna1(String warna1){
+        this.warna1 = warna1;
+    }
+  
+    public  String getWarna2(){
+        return this.warna1;
+    }
+    
+    public void setWarna2(String warna2){
+        this.warna2 = warna2;
+    }
+  
+    
+   
+    
+    
+}
+
---
+++ Agus-khun1/Main.java
@@ -0,0 +1,57 @@
+
+package Tugas1;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+                      System.out.println("###############################");
+	    System.out.println("##  ##### ####  #   # #####  ##");
+	    System.out.println("##  #   # #     #   # #      ##");
+	    System.out.println("##  ##### ##### #   # #####  ##");
+	    System.out.println("##  #   # #   # #   #     #  ##");
+	    System.out.println("##  #   # ##### ##### #####  ##");
+	    System.out.println("###############################");
+            
+                      System.out.println("### WELCOME TO TUGASKU WKWKW ##");
+        
+                       Gantiwarna objek = new Gantiwarna();
+        
+                        Scanner input = new Scanner(System.in);
+                        
+                        System.out.print("Masukan nama pertama = ");
+                        String nama1 = input.nextLine();
+                        objek.setNama1(nama1);
+                        
+                        System.out.print("Masukan warna yang di sukai = ");
+                        String warna1 = input.nextLine();
+                        objek.setWarna1(warna1);
+                        
+                        System.out.println("###############################");
+                        
+                        System.out.print("Masukan nama kedua = ");
+                        String nama2 = input.nextLine();
+                        objek.setNama2(nama2);
+                        
+                         System.out.print("Masukan warna yang di sukai = ");
+                         String warna2 = input.nextLine();
+                         objek.setWarna2(warna2);
+                        
+                          System.out.println("###############################");
+                          
+                          System.out.println("nama pertama adalah = "+objek.getNama1());
+                          System.out.println("warna yang disukai = "+objek.getWarna1());
+                          
+                           System.out.println("###############################");
+                           
+                          System.out.println("nama kedua adalah = "+objek.getNama2());
+                          System.out.println("warna yang disukai = "+objek.getWarna2());
+                          
+                          
+    }
+    
+}
+
