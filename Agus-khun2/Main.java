68ace47938080619230081b62dea2526086061b4
nursyahranirani <nursyahranirani05@gmail.com>
2020-03-25 21:32:41 +0800

    perbaikan error pencarian

--- Agus-khun2/Main.java
+++ Agus-khun2/Main.java
@@ -3,96 +3,55 @@ package Tugas2;
 
 import java.util.Scanner;
 
+public class Array {
 
-public class Main {
-
-    
+   
     public static void main(String[] args) {
-        
-        int cariangka ;
-        boolean ditemukan = false;
-        
-        int[] angka = new int[5];
-        
-        Scanner input = new Scanner(System.in);
-        do{
-            System.out.print("## Masukan angka pertama = ");
-            if(input.hasNextInt()){
-              angka[0] = input.nextInt();
-              ditemukan = true;
-              do{
-                  System.out.print("## Masukan angka ke dua = ");
-                  if(input.hasNextInt()){
-                      angka[1] = input.nextInt();
-                      ditemukan = true;
-                      do{
-                                System.out.print("## Masukan angka ke tiga = ");
-                                        if(input.hasNextInt()){
-                                            angka[2] = input.nextInt();
-                                            ditemukan = true;
-                                            do{
-                                                System.out.print("## Masukan angka ke empat = ");
-                                                if(input.hasNextInt()){
-                                                    angka[3] = input.nextInt();
-                                                    ditemukan = true;
-                                                    do{
-                                                            System.out.print("## Masukan angka ke lima = ");
-                                                            if(input.hasNextInt()){
-                                                                angka[4] = input.nextInt();
-                                                                ditemukan = true;
-                                                                
-                                                                    //  PENCARIAN DATA ARRAY
-                                                                    System.out.print("## Cari angka yang di inginkan = ");
-                                                                    cariangka = input.nextInt();
-                                                                    int j;
-                                                                    
-                                                                    for(j = 0; j<angka.length; j++){
-                                                                        if(angka[j] == cariangka ){
-                                                                            ditemukan = true;
-                                                                            break;
-                                                                        }
-                                                                    }
-                                                                    
-                                                                    if(ditemukan){
-                                                                        System.out.println("angka "+angka[j]+ " Berada pada inputan ke "+angka.length + " Dan index ke = " + j);
-                                                                    }else{
-                                                                        System.out.println("Angka tidak di temukan :) ");
-                                                                    }
-                                                                
-                                                            }else{
-                                                                  System.out.println("Inputan hanya boleh angka ");
-                                                              ditemukan = false;
-                                                              input.next();
-                                                            }
-                                                        }while(!ditemukan);
-                                                }else{
-                                                      System.out.println("Inputan hanya boleh angka ");
-                                                  ditemukan = false;
-                                                  input.next();
-                                                }
-                                            }while(!ditemukan);
-                                        }else{
-                                                 System.out.println("Inputan hanya boleh angka ");
-                                                ditemukan = false;
-                                                input.next();
-                                              }
-                            }while(!ditemukan);
-                  }else{
-                        System.out.println("Inputan hanya boleh angka ");
-                    ditemukan = false;
-                    input.next();
-                  }
-              }while(!ditemukan);
-          }else{
-                System.out.println("Inputan hanya boleh angka ");
-                ditemukan = false;
-                input.next();
+            int[] angka = new int[5];
+            int cari_angka = 0;
+            boolean kondisi = false;
+            int j = 0;
+            Scanner input = new Scanner(System.in);
+            
+            for(int i = 0; i<angka.length; i++){
+                do{
+                    System.out.print("Masukan angka ke "+(i+1)+" = ");
+                    if(input.hasNextInt()){
+                        angka[i] = input.nextInt();
+                        kondisi = true;
+                    }else{
+                        System.out.println("Inputan hanya boleh angka");
+                        kondisi = false;
+                        input.next();
+                    }
+                }while(!kondisi);
             }
-        }while(!ditemukan);
- 
-
-     
-    }
-    
+            
+           
+           do{
+               System.out.print("Masukan angka yang ingin di cari = ");
+              if(input.hasNextInt()){
+                  cari_angka = input.nextInt();
+                  kondisi = true;
+                  for( j = 0; j <angka.length; j++){
+                      if(cari_angka == angka[j]){
+                          kondisi = true;
+                          break;
+                      }
+                  }
+              }else{
+                  input.next();
+                  kondisi = false;
+              }
+           }while(!kondisi);
+        
+           try{
+               if(kondisi == true){
+                    System.out.println("Angka "+angka[j]+" Berada pada inputan "+(j+1)+" dan index ke = "+j);
+               }
+           }catch(IndexOutOfBoundsException e){
+               System.out.println("angka tidak ada");
+           }
+           
+ }
 }
-
